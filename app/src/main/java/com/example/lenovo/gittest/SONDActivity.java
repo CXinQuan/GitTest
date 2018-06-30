51a4d90b1d038b73f69a224dfb88d34b9c8b07df
CXinQuan <1611205417@qq.com>
2018-06-30 20:59:13 +0800

    添加了第二个界面

---
+++ app/src/main/java/com/example/lenovo/gittest/SONDActivity.java
@@ -0,0 +1,23 @@
+package com.example.lenovo.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SONDActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView text=new TextView(this);
+
+        TextView text2=new TextView(this);
+
+        TextView text3=new TextView(this);
+        Button btn=new Button(this);
+
+
+    }
+
+}
