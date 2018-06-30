6ae2a27bb2399bfa2077573b340723427271ddff
CXinQuan <1611205417@qq.com>
2018-06-30 20:26:31 +0800

    第二次提交

--- app/src/main/java/com/example/lenovo/gittest/MainActivity.java
+++ app/src/main/java/com/example/lenovo/gittest/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.lenovo.gittest;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text=new TextView(this);
+
+
+
     }
 }
